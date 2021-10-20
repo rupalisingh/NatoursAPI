f232982467ceb215006bc3ae23c0bd87105240d9
Rupali Singh <rupalisingh2304@gmail.com>
2021-10-20 13:26:47 +0530

    Add new tour

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const fs = require('fs')
-const express = require('express')
+const fs = require("fs");
+const express = require("express");
 
-const app = express()
+const app = express();
+
+//MiddleWare
+app.use(express.json());
 
 // app.get('/', (req,res) => {
 // res.status(200).send('Hello from the server')
@@ -12,26 +15,44 @@ const app = express()
 // app.get('/', (req,res) => {
 //     res.status(200).json({message : 'Hello from the server', app : 'Natours-udemy'})
 //     })
-   
+
 // app.post('/', (req, res) => {
 //     res.send("You can post this to endpoint....")
 // })
 
-const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours-simple.json'))
+const tours = JSON.parse(fs.readFileSync("./dev-data/data/tours-simple.json"));
 
-app.get('/api/v1/tours', (req,res) => {
-res.status(200).json({
-    status : 'success',
-    results : tours.length,
-    data : {
-        tours : tours
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  //console.log(req.body)
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    "./dev-data/data/tours-simple.json",
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
     }
-})
-})
+  );
+});
 
-const port = 3000
+const port = 3000;
 
 app.listen(port, () => {
-    console.log(`App running on port ${port}...`)
-})
-
+  console.log(`App running on port ${port}...`);
+});
