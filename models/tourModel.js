3a9429034058b4ae5200b73293a8809cf62e5b74
Rupali Singh <rupalisingh2304@gmail.com>
2021-10-23 22:40:56 +0530

    Complete modeles for tour Schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,38 +1,68 @@
 const mongoose = require("mongoose");
 
-
 // Describing a Schema
 
 const tourSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: [true, "A tour must have a name"],
-      unique: true,
-    },
-    durations : {
-      type : Number,
-      required : [true, "A tour must have a duration"]
-    },
-    MaxGrpupSize : {
-      type : Number,
-      required : [true, 'A tour must have a group size']
-    },
-    difficulty : {
-      type : String,
-      required : [true, 'A tour must have a difficulty']
-    },
-    rating: {
-      type: Number,
-      default: 4.5,
-    },
-    price: {
-      type: Number,
-      required: [true, "A tour must have a price"],
-    },
-  });
-  
-  // Creating model
-  
-  const Tour = mongoose.model("Tour", tourSchema);
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+    trim : true,
+  },
+  durations: {
+    type: Number,
+    required: [true, "A tour must have a duration"],
+  },
+  MaxGrpupSize: {
+    type: Number,
+    required: [true, "A tour must have a group size"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A tour must have a difficulty"],
+  },
+  ratingsAvverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+  priceDiscount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required :  [true, "A tour must have a description"]
+  },
+  description : {
+    type : String,
+    trim : true,
+  },
+  imageCover : {
+    type : String,
+    required : [true, "A tour must have a cover Image"],
+  },
+  images : {
+    type : [String],
+  },
+  createdAt : {
+    type : Date,
+    default : Date.now(),
+  },
+  startDates : {
+    type : [Date],
+  }
+});
 
-  module.exports = Tour
+// Creating model
+
+const Tour = mongoose.model("Tour", tourSchema);
+
+module.exports = Tour;
