0a8e3d3838ed45ce87da187741aa0283e34d29a5
Rupali Singh <rupalisingh2304@gmail.com>
2021-10-27 22:45:46 +0530

    Pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -77,6 +77,18 @@ exports.getAllTours = async (req, res) => {
       query.select('-__v')   // Excluding this v field using 'minus'
     }
 
+    //5)Pagination
+    //?page=2&limit=10
+    const page = req.query.page * 1 || 1
+    const limit = req.query.limit * 1 || 100
+    const skip = (page - 1) * limit
+    query = query.skip(skip).limit(limit)
+
+    if(req.query.page){
+      const numTours = await Tour.countDocuments()
+      if(skip >= numTours) throw new Error('This page does not exist')
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
