77099d74d257755e8f6bb6bd6926ec6d3ccb83ec
Rupali Singh <rupalisingh2304@gmail.com>
2021-10-29 22:10:03 +0530

    Handle Duplicate database Field error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,18 @@
 /* eslint-disable prefer-const */
 /* eslint-disable node/no-unsupported-features/es-syntax */
+const AppError = require("../utils/appError")
 
 const handleCasteErrorDB = (err) => {
   const message = `Invalid ${err.path} : ${err.value}`;
   return new AppError(message, 400);
 };
+
+const handleDuplicateFieldsDB = (err) => {
+    const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0]
+    const message = `Duplicate field values : ${value}. Please use another value!`
+    return new AppError(message, 400);
+
+}
 
 const sendErrorForDEv = (err, res) => {
   res.status(err.statusCode).json({
@@ -44,7 +52,8 @@ module.exports = (err, req, res, next) => {
     sendErrorForDEv(err, res);
   } else if (process.env.NODE_ENV === "production") {
     let error = { ...err };
-    if (error.name === "CastError") err = handleCasteErrorDB(err);
+    if (error.name === "CastError") error = handleCasteErrorDB(error);
+    if(error.code === 11000) error = handleDuplicateFieldsDB(error)
 
     sendErrorForProd(error, res);
   }
