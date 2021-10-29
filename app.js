0df76f884ded7093b22f3619a3b40d3d49e1ad39
Rupali Singh <rupalisingh2304@gmail.com>
2021-10-29 12:36:44 +0530

    Handling unhandled Routes

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 const express = require("express");
 const morgan = require("morgan");
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
 //1) MiddleWare
 
-if(process.env.NODE_ENV === 'development'){
-
-    app.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
 // Creating Own MiddleWare
-app.use((req, res, next) => {
-  console.log("Hello From the Middleware");
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log("Hello From the Middleware");
+//   next();
+// });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -40,10 +39,7 @@ app.use((req, res, next) => {
 //     res.send("You can post this to endpoint....")
 // })
 
-
 // 2) Route Handlers
-
-
 
 //app.get("/api/v1/tours", getAllTours);
 
@@ -58,9 +54,14 @@ app.use((req, res, next) => {
 //3) Route
 // 3.1) Mounting router
 
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
-
-
