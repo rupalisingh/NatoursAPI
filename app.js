046690056c6de450f5c261c549c3104bc575d3fa
Rupali Singh <rupalisingh2304@gmail.com>
2021-12-04 23:19:37 +0530

    Set security HTTP headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet")
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -11,10 +12,15 @@ const app = express();
 
 //1) Global MiddleWare
 
+// Set Securtiy HTTP headers
+app.use(helmet())
+
+// Development logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Implement Rate limiting from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -23,7 +29,11 @@ const limiter = rateLimit({
 
 app.use('/api',limiter);
 
-app.use(express.json());
+// Body Parser, reading data from body into req.body
+
+app.use(express.json({limit : '10kb'}));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 // Creating Own MiddleWare
